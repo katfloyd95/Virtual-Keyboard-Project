5fe33e2884479e9cc197c27f25ba21bd9a1b4456
Katarina <katarina.floyd@getvoila.com>
2022-09-21 00:22:09 +0200

    add tiles for wordle, add some functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const tileDisplay = document.querySelector('.title-container');
+const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.keyboard-container');
+
+const wordle = "SUPER";
 
 const keys = [
   'Q',
@@ -32,14 +34,45 @@ const keys = [
   '«'
 ]
 
-function handleClick() {
-  console.log('clicked');
-}
+const guessRows = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', '']
+]
+
+let currentRow = 0;
+let currentTile = 0;
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement('div');
+  rowElement.setAttribute('id', 'guessRow-' + guessRowIndex);
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement('div');
+    tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex);
+    tileElement.classList.add('tile');
+    rowElement.append(tileElement);
+  })
+  tileDisplay.append(rowElement);
+})
 
 keys.forEach(key => {
   const buttonElement = document.createElement('button');
   buttonElement.textContent = key;
   buttonElement.setAttribute('id', key);
-  buttonElement.addEventListener('click', handleClick);
+  buttonElement.addEventListener('click', () => handleClick(key));
   keyboard.append(buttonElement);
 })
+
+function handleClick(key)) {
+  console.log('clicked', key);
+  // addLetter(letter);
+}
+
+// function addLetter(letter) {
+//   console.log("letter added");
+//   const selectedTile = document.getElementById('guessRow-' + currentRow '-tile-' + currentTile);
+//   selectedTile.textContent = letter;
+// }
